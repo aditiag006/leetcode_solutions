6e6013fe4993dc94cc2985141aac6121265e8999
aditiag006 <aditiagarwal1308@gmail.com>
2026-09-14 16:55:42 +0530

    Time: 0 ms (100%), Space: 8.7 MB (45.52%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int addDigits(int num) {
+
+        if(num <10) return num;
+
+        int sum = 0;
+
+        while(num > 0){
+            int rem = num%10;
+            sum += rem;
+            num = num/10;
+        }
+
+        int ans = addDigits(sum);
+
+        return ans;
+        
+    }
+};
