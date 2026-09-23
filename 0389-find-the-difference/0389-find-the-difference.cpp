096949b6ff08d86a3a24a5d1aabd6aec16a77f5e
aditiag006 <aditiagarwal1308@gmail.com>
2026-09-23 11:02:16 +0530

    Time: 0 ms (100%), Space: 9.3 MB (50.19%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] != t[i]) return t[i];
+        }
+
+        if(t.size() > 1){
+            return t[t.size() - 1];
+        }else{
+            return t[0];
+        }
+        
+    }
+};
